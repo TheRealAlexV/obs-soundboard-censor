43294a3084fcf818dcd7b8e36c66e880a8ad5f90
TheRealAlexV <alex.vanino@gmail.com>
2026-07-21 18:11:29 -0400

    Fix CI: flexible FFmpeg finding, proper install paths
    - FindLibAv.cmake: check if local .deps/ dir exists, fall back
      to find_library/find_path and pkg-config
    - Use GNUInstallDirs for platform-agnostic install paths
    - Simplify Linux CI: skip cmake --install, just use package target

--- cmake/FindLibAv.cmake
+++ cmake/FindLibAv.cmake
@@ -20,14 +20,7 @@ function(find_libav)
   if(NOT buildspec)
     file(READ "${CMAKE_CURRENT_SOURCE_DIR}/buildspec.json" buildspec)
   endif()
-  string(
-    JSON
-    version
-    GET
-    ${buildspec}
-    dependencies
-    prebuilt
-    version)
+  string(JSON version GET ${buildspec} dependencies prebuilt version)
 
   if(MSVC)
     set(arch ${CMAKE_GENERATOR_PLATFORM})
@@ -36,26 +29,48 @@ function(find_libav)
   endif()
   set(deps_root "${CMAKE_CURRENT_SOURCE_DIR}/.deps/obs-deps-${version}-${arch}")
 
-  target_include_directories(${CMAKE_PROJECT_NAME} PRIVATE "${deps_root}/include")
-  target_link_libraries(
-    ${CMAKE_PROJECT_NAME}
-    PRIVATE "${deps_root}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}avcodec${CMAKE_STATIC_LIBRARY_SUFFIX}"
-            "${deps_root}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}avformat${CMAKE_STATIC_LIBRARY_SUFFIX}"
-            "${deps_root}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}avutil${CMAKE_STATIC_LIBRARY_SUFFIX}"
-            "${deps_root}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}swresample${CMAKE_STATIC_LIBRARY_SUFFIX}")
+  if(EXISTS "${deps_root}")
+    target_include_directories(${CMAKE_PROJECT_NAME} PRIVATE "${deps_root}/include")
+    target_link_libraries(
+      ${CMAKE_PROJECT_NAME}
+      PRIVATE "${deps_root}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}avcodec${CMAKE_STATIC_LIBRARY_SUFFIX}"
+              "${deps_root}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}avformat${CMAKE_STATIC_LIBRARY_SUFFIX}"
+              "${deps_root}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}avutil${CMAKE_STATIC_LIBRARY_SUFFIX}"
+              "${deps_root}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}swresample${CMAKE_STATIC_LIBRARY_SUFFIX}")
 
-  if(MSVC)
-    # find libav .dlls
-    file(
-      GLOB_RECURSE av_dlls
-      LIST_DIRECTORIES false
-      "${deps_root}/bin/av*.dll")
-    # add sw* .dlls
-    file(
-      GLOB_RECURSE sw_dlls
-      LIST_DIRECTORIES false
-      "${deps_root}/bin/sw*.dll")
-    # install .dlls to the same directory as the executable
-    install(FILES ${av_dlls};${sw_dlls} DESTINATION "obs-plugins/64bit")
+    if(MSVC)
+      file(GLOB_RECURSE av_dlls LIST_DIRECTORIES false "${deps_root}/bin/av*.dll")
+      file(GLOB_RECURSE sw_dlls LIST_DIRECTORIES false "${deps_root}/bin/sw*.dll")
+      install(FILES ${av_dlls};${sw_dlls} DESTINATION "obs-plugins/64bit")
+    endif()
+    return()
+  endif()
+
+  find_path(AVCODEC_INCLUDE_DIR libavcodec/avcodec.h)
+  find_path(AVFORMAT_INCLUDE_DIR libavformat/avformat.h)
+  find_path(AVUTIL_INCLUDE_DIR libavutil/avutil.h)
+  find_path(SWRESAMPLE_INCLUDE_DIR libswresample/swresample.h)
+
+  find_library(AVCODEC_LIBRARY avcodec)
+  find_library(AVFORMAT_LIBRARY avformat)
+  find_library(AVUTIL_LIBRARY avutil)
+  find_library(SWRESAMPLE_LIBRARY swresample)
+
+  if(AVCODEC_LIBRARY AND AVFORMAT_LIBRARY AND AVUTIL_LIBRARY AND SWRESAMPLE_LIBRARY)
+    target_include_directories(${CMAKE_PROJECT_NAME} PRIVATE
+      ${AVCODEC_INCLUDE_DIR} ${AVFORMAT_INCLUDE_DIR}
+      ${AVUTIL_INCLUDE_DIR} ${SWRESAMPLE_INCLUDE_DIR})
+    target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE
+      ${AVCODEC_LIBRARY} ${AVFORMAT_LIBRARY}
+      ${AVUTIL_LIBRARY} ${SWRESAMPLE_LIBRARY})
+  else()
+    find_package(PkgConfig QUIET)
+    if(PkgConfig_FOUND)
+      pkg_check_modules(FFMPEG REQUIRED IMPORTED_TARGET
+        libavformat libavcodec libavutil libswresample)
+      target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE PkgConfig::FFMPEG)
+    else()
+      message(FATAL_ERROR "FFmpeg libraries not found")
+    endif()
   endif()
 endfunction(find_libav)
